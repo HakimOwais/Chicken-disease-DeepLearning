925fdb2dc5061889edfcc9683f5879272d041815
HakimOwais <owaisibnmushtaq@gmail.com>
2024-01-10 18:10:56 +0530

    prepare base odel pipeline

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # logger.info("Welcome to my custom log")
 
 STAGE_NAME = 'Data Ingestion stage'
@@ -10,6 +10,19 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare base model"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>> stage {STAGE_NAME} started <<<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
